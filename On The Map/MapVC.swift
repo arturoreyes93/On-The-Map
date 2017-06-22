200162d1389d1ade73690f67743d595490178703
Arturo Reyes <reyesm93@gmail.com>
2017-06-21 20:44:32 -0500

    Add map vc and view outlet

---
+++ On The Map/MapVC.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  On The Map
+//
+//  Created by Arturo Reyes on 6/21/17.
+//  Copyright © 2017 Arturo Reyes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+
+class MapVC: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapVIew: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
