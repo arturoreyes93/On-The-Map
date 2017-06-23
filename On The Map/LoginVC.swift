d464cefc74686f81008553a2b5e0e9cddf8afc58
Arturo Reyes <reyesm93@gmail.com>
2017-06-23 15:07:17 -0500

    Set text fields functions

--- On The Map/LoginVC.swift
+++ On The Map/LoginVC.swift
@@ -21,6 +21,8 @@ class LoginVC: UIViewController, UITextFieldDelegate, UINavigationControllerDele
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configure(username)
+        configure(password)
     }
     
     
@@ -31,8 +33,35 @@ class LoginVC: UIViewController, UITextFieldDelegate, UINavigationControllerDele
     }
     
     
-    
     let logInTextAttributes:[String:Any] = [NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Arial", size: 18)!]
+    
+    func configure(_ textField: UITextField) {
+        textField.delegate = self
+        textField.defaultTextAttributes = logInTextAttributes
+        textField.textAlignment = NSTextAlignment.left
+        
+        if textField == username {
+            textField.text = "Email"
+        } else if textField == password {
+            textField.text = "Password"
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "Email" || textField.text == "Password" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text == "" {
+            if textField == username {
+                username.text = "Email"
+            } else if textField == password {
+                password.text = "Password"
+            }
+        }
+    }
     
     
 
