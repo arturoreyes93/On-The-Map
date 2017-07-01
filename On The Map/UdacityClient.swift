3d76f9fcd4006a82a2ddda51500fab12f83a9fb7
Arturo Reyes <reyesm93@gmail.com>
2017-06-30 20:42:18 -0500

    Create tastForGetMethod func

--- On The Map/UdacityClient.swift
+++ On The Map/UdacityClient.swift
@@ -20,47 +20,11 @@ class UdacityClient : NSObject {
         super.init()
     }
     
-    func getUserData() {
-        let request = URLRequest(url: URL(string: "https://www.udacity.com/api/users/\(userKey!)")!)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if error != nil {
-                print(error)
-            }
-            let range = Range(5..<data!.count)
-            let newData = data?.subdata(in: range) /* subset response data! */
-            print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
-            
-            let parsedResult: [String:AnyObject]!
-            do {
-                parsedResult = try JSONSerialization.jsonObject(with: newData!, options: .allowFragments) as! NSDictionary as! [String : AnyObject]
-            } catch {
-                print("Could not parse the data as JSON: '\(newData)'")
-                return
-            }
-            
-            guard (parsedResult["error"] == nil) else {
-                print(parsedResult["error"])
-                return
-            }
-            
-            guard let userInfo = parsedResult["user"] as? NSDictionary else {
-                return
-            }
-            
-            self.firstName = userInfo["first_name"] as! String
-            self.lastName = userInfo["last_name"] as! String
-            print(self.firstName!)
-            print(self.lastName!)
-        }
-        
-        task.resume()
-    }
-    
-    private func urlFromParameters(_client: String, paremeters: [String:AnyObject], withPathExtension: String? = nil) -> URL {
+    func urlFromParameters(client: String, paremeters: [String:AnyObject], withPathExtension: String? = nil) -> URL {
         
         var components = URLComponents()
         
-        if _client == "udacity" {
+        if client == "udacity" {
             components.scheme = Constants.Udacity.APIScheme
             components.host = Constants.Udacity.APIHost
             components.path = Constants.Udacity.APIPath
@@ -80,6 +44,102 @@ class UdacityClient : NSObject {
         return components.url!
         
     }
+    
+    func convertDataWithCompletionHandler(_ data: Data, completionHandlerForConvertData: (_ result: AnyObject?, _ error: NSError?) -> Void) {
+        
+        var parsedResult: AnyObject! = nil
+        do {
+            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+            completionHandlerForConvertData(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
+        
+        completionHandlerForConvertData(parsedResult, nil)
+    }
+    
+    func getUserData() {
+        let request = URLRequest(url: URL(string: "https://www.udacity.com/api/users/\(userKey!)")!)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if error != nil {
+                print(error!)
+            }
+            let range = Range(5..<data!.count)
+            let newData = data?.subdata(in: range) /* subset response data! */
+            print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
+            
+            let parsedResult: [String:AnyObject]!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: newData!, options: .allowFragments) as! NSDictionary as! [String : AnyObject]
+            } catch {
+                print("Could not parse the data as JSON: '\(newData)'")
+                return
+            }
+            
+            guard (parsedResult["error"] == nil) else {
+                print(parsedResult["error"]!)
+                return
+            }
+            
+            guard let userInfo = parsedResult["user"] as? NSDictionary else {
+                return
+            }
+            
+            self.firstName = userInfo["first_name"] as? String
+            self.lastName = userInfo["last_name"] as? String
+            print(self.firstName!)
+            print(self.lastName!)
+        }
+        
+        task.resume()
+    }
+    
+    func taskForGetMethod(client: String, method: String, parameters: [String:AnyObject], completionHandlerForGET: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionTask {
+        
+        let request = NSMutableURLRequest(url: urlFromParameters(client: client, paremeters: parameters, withPathExtension: method))
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            func sendError(_ error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error]
+                completionHandlerForGET(nil, NSError(domain: "taskForGETMethod", code: 1, userInfo: userInfo))
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard data != nil else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            if client == "udacity" {
+                let range = Range(5..<data!.count)
+                let newData = data!.subdata(in: range) /* subset response data! */
+                print(NSString(data: newData, encoding: String.Encoding.utf8.rawValue)!)
+                self.convertDataWithCompletionHandler(newData, completionHandlerForConvertData: completionHandlerForGET)
+            } else {
+                self.convertDataWithCompletionHandler(data!, completionHandlerForConvertData: completionHandlerForGET)
+            }
+        }
+        
+        task.resume()
+        
+        return task
+        
+    }
+    
+    
     
     class func sharedInstance() -> UdacityClient {
         struct Singleton {
