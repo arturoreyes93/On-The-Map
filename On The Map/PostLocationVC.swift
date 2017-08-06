e9f03dcdc9e8521b9ba35d99c5d6f3b21c3727c2
Arturo Reyes <reyesm93@gmail.com>
2017-08-05 19:24:47 -0500

    Set PostLocationVC UI
    still need to implement methods, add activity indicator, alert message
    and cancel button

--- On The Map/PostLocationVC.swift
+++ On The Map/PostLocationVC.swift
@@ -10,41 +10,65 @@ import Foundation
 import UIKit
 import MapKit
 
-class PostLocationVC: UIViewController {
-    
-    @IBOutlet weak var locationTextField: UITextField!
-    @IBOutlet weak var websiteTextField: UITextField!
-    @IBOutlet weak var findLocationButton: UIButton!
-    
+class PostLocationVC: UIViewController, MKMapViewDelegate  {
+
+    @IBOutlet weak var locationSubview: UIView!
+    @IBOutlet weak var mapSubview: UIView!
+    @IBOutlet weak var locationText: UITextField!
+    @IBOutlet weak var websiteText: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+ 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        configure(locationTextField)
-        configure(websiteTextField)
+
+        configure(locationText)
+        configure(websiteText)
     }
     
-    func getLocationFromAddress(_ location: String, _ completionHandlerForGetLocation: @escaping (_ success: Bool, _ studentLocation: MKPlacemark, _ studentCoordinate: CLLocationCoordinate2D, _ errorString: String?)) {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setFindView()
+    }
+    
+    func setFindView() {
+        locationSubview.isHidden = false
+        mapSubview.isHidden = true
+    }
+    
+    func setMapView() {
+        locationSubview.isHidden = true
+        mapSubview.isHidden = false
+    }
+    
+    @IBAction func findLocation(_ sender: Any) {
+        
+        getLocationFromAddress(locationText.text!)
+        
+    }
+    
+    func getLocationFromAddress(_ location: String) {
         
         let geoCoder = CLGeocoder()
         
-        geoCoder.geocodeAddressString(location) { (placemark: [CLPlacemark]?, error: NSError?) in
-            if let placemark = placemark?[0] {
-                let studentLocation = MKPlacemark(placemark: placemark)
-                let studentCoordinate = placemark.location!.coordinate as CLLocationCoordinate2D
+        geoCoder.geocodeAddressString(location) { placemarks, error in
+            if let placemark = placemarks?[0] {
+                self.mapView.addAnnotation(MKPlacemark(placemark: placemark))
+                let coordinate = placemark.location!.coordinate as CLLocationCoordinate2D
+                let span = MKCoordinateSpanMake(0.15, 0.15)
+                let region = MKCoordinateRegion(center: coordinate, span: span)
+                self.mapView.setRegion(region, animated: true)
+                self.setMapView()
+            
+            } else {
+                print("error with finding address")
                 
             }
         }
-    }
-    
-    func setLocationRegion(_ location: CLLocationCoordinate2D) {
-        let span = MKCoordinateSpan(-.15, 0.15)
-        let region = MKCoordinateRegion(center: location, span: span)
         
     }
-    
-    
-    
-    
     
 
 }
@@ -53,9 +77,9 @@ extension PostLocationVC: UITextFieldDelegate {
     
     func configure(_ textField: UITextField) {
         textField.delegate = self
-        textField.defaultTextAttributes = [NSForegroundColorAttributeName: UIColor.gray, NSFontAttributeName: UIFont(name: "Arial", size: 18)!]
-        textField.textAlignment = NSTextAlignment.left
-        
+        textField.defaultTextAttributes = [NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont(name: "Arial", size: 32)!]
+        textField.textAlignment = NSTextAlignment.center
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.gray])
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
